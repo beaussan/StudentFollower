2fc72761cce95a483cf4a4717c21b19fa5473aa5
Nicolas Beaussart <nicolas.beaussart@etudiant.univ-lille1.fr>
2015-05-26 10:59:27 +0200

    Adding string list

--- src/org/studentfolower/util/ArrayUtils.java
+++ src/org/studentfolower/util/ArrayUtils.java
@@ -1,10 +1,10 @@
 package org.studentfolower.util;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayUtils {
 
-	public static String[] toArray(ArrayList<String> str) {
+	public static String[] toArray(List<String> str) {
 		String[] stockArr = new String[str.size()];
 		stockArr = str.toArray(stockArr);
 		return stockArr;
