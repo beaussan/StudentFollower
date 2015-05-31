b42fd9f4a8f8fccac46237782f913f14c098b8a4
MoulardS <fappingmaterial@hotmail.fr>
2015-05-31 20:37:34 +0200

    Cleaning the stuff I spent two years writing and that's useless now that I found how to do it in three lines :(

--- src/org/studentfolower/ihm/Historique.java
+++ src/org/studentfolower/ihm/Historique.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -34,68 +35,19 @@ public class Historique extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setUndecorated(true);
 		
-		/*
-		String daystr[] = new String[31];
-		for (int i = 1; i <= 31; i++) {
-			if (i < 10) {
-				daystr[i - 1] = "0" + i;
-			} else {
-				daystr[i - 1] = "" + i;
-			}
-		}
-
-		String monthstr[] = new String[12];
-		for (int j = 1; j <= 12; j++) {
-			if (j < 10) {
-				monthstr[j - 1] = "0" + j;
-			} else {
-				monthstr[j - 1] = "" + j;
-			}
-		}
-
-		String yearstr[] = new String[16];
-		int k = 2000;
-		for (int l = 0; l < 16; l++) {
-			int cat = k + l;
-			yearstr[l] = "" + cat;
-		}
-
-		JComboBox<String> day = new JComboBox<String>(daystr);
-		JComboBox<String> month = new JComboBox<String>(monthstr);
-		JComboBox<String> year = new JComboBox<String>(yearstr);
-		*/
-
-		/*
-		JPanel boxPanel = new JPanel();
-		boxPanel.setLayout(new GridLayout(1, 3));
-		boxPanel.add(day);
-		boxPanel.add(month);
-		boxPanel.add(year);
-		this.add(boxPanel, BorderLayout.NORTH);
-		*/
-		
 		this.setLayout(new BorderLayout());
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		buttonPanel.add(back);
 		this.add(buttonPanel, BorderLayout.SOUTH);
 		
-		/*UtilDateModel model = new UtilDateModel();
-		Properties p = new Properties();
-		p.put("text.today", "Aujourd'hui");
-		p.put("text.month", "Mois");
-		p.put("text.year", "Année");
-		JDatePanelImpl datePanel = new JDatePanelImpl(model,p);
-		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel,null);
-		Date selectedDate = (Date) datePicker.getModel().getValue();
-
-		this.add(datePicker,BorderLayout.NORTH);*/
-		
 		JXDatePicker picker = new JXDatePicker();
         picker.setDate(Calendar.getInstance().getTime());
         picker.setFormats(new SimpleDateFormat("dd.MM.yyyy"));
 
         this.add(picker,BorderLayout.NORTH);
+        
+        //Date date = picker.getDate();
 
 		pack();
 		this.setVisible(true);
