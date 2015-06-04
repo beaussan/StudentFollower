ce38c7694d72e037f926e40ecbbbdd7a848f8773
Nicolas Beaussart <nicolas.beaussart@etudiant.univ-lille1.fr>
2015-06-04 08:49:55 +0200

    Voila, ta fonction, VOILA

--- src/org/studentfolower/util/ArrayUtils.java
+++ src/org/studentfolower/util/ArrayUtils.java
@@ -1,8 +1,15 @@
 package org.studentfolower.util;
 
+import java.util.Collection;
 import java.util.List;
 
 public class ArrayUtils {
+
+	public static String[] toArray(Collection<String> str) {
+		String[] stockArr = new String[str.size()];
+		stockArr = str.toArray(stockArr);
+		return stockArr;
+	}
 
 	public static String[] toArray(List<String> str) {
 		String[] stockArr = new String[str.size()];
